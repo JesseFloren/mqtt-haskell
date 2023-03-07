f0499214959b3f8349a3bfd3f89e3a504a331880
JanMouwes <jan.mouwes@gmail.com>
2023-03-07 20:19:58 +0100

    very basic implementation of the loop of a chat app

---
+++ lib/Chat/App.hs
@@ -0,0 +1,58 @@
+module Chat.App (run) where
+
+import Chat.Message (Message(..))
+import System.IO (hSetBuffering, stdout, BufferMode(LineBuffering))
+import System.Console.ANSI (cursorUp, clearLine, clearScreen, setCursorPosition)
+
+type Chat = [Message]
+
+data AppState = AppState {username :: String, chat :: Chat}
+
+emptyState :: String -> AppState
+emptyState u = AppState u []
+
+prompt :: String -> IO String
+prompt q = do
+  putStrLn q
+  a <- getLine
+  cursorUp 1
+  clearLine
+  cursorUp 1
+  clearLine
+  return a
+
+resetScreen :: IO ()
+resetScreen = do
+  clearScreen
+  setCursorPosition 0 0
+
+login :: IO String
+login = prompt "What is your username?"
+
+printStateInfo :: AppState -> IO ()
+printStateInfo state = putStrLn $ "Logged in as " ++ username state
+
+run :: IO ()
+run = do
+  hSetBuffering stdout LineBuffering
+  resetScreen
+  state <- emptyState <$> login
+  printStateInfo state
+  runLoop state
+  where 
+    runLoop :: AppState -> IO ()
+    runLoop state = do
+      let chat' = chat state
+      putStr (showChat chat')
+      msg <- getLine
+      cursorUp (2 + length chat')
+      putStrLn ""
+      runLoop (sendMessage state msg)
+
+
+sendMessage :: AppState -> String -> AppState
+sendMessage (AppState user chat) msg = 
+  AppState user (Message {message = msg, author = user} : chat)
+
+showChat :: Chat -> String
+showChat = unlines . map show . reverse 
