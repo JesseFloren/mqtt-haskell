70ae2e42cc61212384e1b9b2cc890b4ae8c2acec
Jesse Floren Vrooman <jessefloren@Jesses-MacBook-Air.local>
2023-03-10 22:15:33 +0100

    Fixed Main.hs change

--- src-client/Main.hs
+++ src-client/Main.hs
@@ -1,8 +1,6 @@
 module Main where
 import Client (runClient)
-import Packets
 
 main :: IO ()
 main = do
-    print $ byteStringToPacket $ packetToByteString (
-        connectMessage "Client12" (ConnectFlags{username=(Just "Username"), password=(Just "Password"), will=(Just (False, One, "topic", "message")), cleanSession=True}) 6000)
+    runClient
