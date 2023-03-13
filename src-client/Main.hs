94c2358cb8f1e1ffbbb6fb0d69a7fc4cb35f2713
Jesse Floren Vrooman <jessefloren@Jesses-MacBook-Air.local>
2023-03-13 23:22:17 +0100

    Added all read functionalities

--- src-client/Main.hs
+++ src-client/Main.hs
@@ -1,8 +1,7 @@
 module Main where
 import Client (runClient)
-import Packets
 
 main :: IO ()
 main = do
-    print $ readPublishPacket $ writePublishPacket 10 (PublishFlags{dup=True, retain=True, channel=("topic", Two)}) "Hello"
+    runClient
     
