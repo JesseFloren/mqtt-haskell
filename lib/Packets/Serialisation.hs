d0120c77c21c6dcdf7d0571d75f0f2f7bb59846e
JanMouwes <jan.mouwes@gmail.com>
2023-03-21 14:17:20 +0100

    complete draft of Serialisation library

--- lib/Packets/Serialisation.hs
+++ lib/Packets/Serialisation.hs
@@ -1,26 +1,60 @@
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE InstanceSigs #-}
 module Packets.Serialisation where
 
 import Packets.Abstract
 import qualified Data.ByteString as C
-import Data.Bits 
-import Data.Bits.Extras
+import qualified Data.ByteString.Char8 as BC
+import Utils.Bits (bitsToInt, bit)
+import Data.Bits (shift, (.|.))
+import Data.Word 
 
 
-class Serialisable a where
-  serialise :: a -> C.ByteString
+class Serialisable s where
+  serialise :: s -> C.ByteString
+  
+(<+>) :: (Serialisable a, Serialisable b) => a -> b -> C.ByteString
+(<+>) a b = serialise a `C.append` serialise b
+
+instance Serialisable s => Serialisable [s] where
+  serialise :: [s] -> C.ByteString
+  serialise = C.concat . map serialise
 
 instance Serialisable Packet where
   serialise :: Packet -> C.ByteString
-  serialise (Packet cmd flags header payload) = 
-    serialise cmd `C.append` serialise flags `C.append` serialise header `C.append` serialise payload
+  serialise (Packet cmd flags header payload) = PHeader cmd flags <+> header <+> payload
+
+-- Necessary because we cannot create an instance for the tuple (CommandType, Flags)
+data PacketHeader = PHeader CommandType Flags
 
 -- | CommandType and Flags are stored in the same byte, so they have a shared instance
-instance Serialisable (CommandType, Flags) where
-  serialise :: (CommandType, Flags) -> C.ByteString
-  serialise (cmd, flags) = serialise bits
+instance Serialisable PacketHeader where
+  serialise :: PacketHeader -> C.ByteString
+  serialise (PHeader cmd flags) = serialise bits
     where
-      bits = (toWord8 cmd `shift` 4) .|. (bitsToInt flags)
+      bits = (toWord8 cmd `shift` 4) .|. (fromIntegral $ bitsToInt flags)
+
+
+-- TODO change to Serialisable [Content] to avoid byte wastage
+instance Serialisable Content where
+  serialise :: Content -> C.ByteString
+  serialise (Str s) = BC.pack s -- avoids overlapping instances
+  serialise (Int16 i) = serialise i
+  serialise (Int8 i) = serialise i
+  serialise (Flags fs) = serialise @Int $ bitsToInt fs
+  serialise (Con b) = serialise @Int $ bitsToInt [bit b]
+  serialise (QoS qos) = serialise @Int $ putQoS qos
 
 instance Serialisable Int where
   serialise :: Int -> C.ByteString
   serialise = C.singleton . fromIntegral
+
+instance Serialisable Word8 where
+  serialise :: Word8 -> C.ByteString
+  serialise = C.singleton
+
+instance Serialisable C.ByteString where
+  serialise :: C.ByteString -> C.ByteString
+  serialise = id
+
+
