d47aa78ce5c4110175feddecaac1d192dfa95632
Jesse Floren Vrooman <jessefloren@Jesses-MacBook-Air.local>
2023-03-30 20:46:04 +0200

    Solved bits test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,12 +1,10 @@
 import Test.Tasty (defaultMain, testGroup)
 import qualified Spec.Sanity as Sanity
 import qualified Spec.Packets as Packets
-import qualified Spec.Utils.Bits as Bits
 
 
 main :: IO ()
 main = defaultMain $ testGroup "mqtt-hs" [
     Sanity.test
   , Packets.test
-  , Bits.test
   ]
