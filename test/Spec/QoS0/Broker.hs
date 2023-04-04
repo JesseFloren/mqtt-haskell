65541d6c029e022843112d720d5d8c5c55a8b953
JanMouwes <jan.mouwes@gmail.com>
2023-04-04 17:25:48 +0200

    add naive Arbitrary Socket instance. Add test to check whether all subscriptions are sent a Packet

--- test/Spec/QoS0/Broker.hs
+++ test/Spec/QoS0/Broker.hs
@@ -1,16 +1,24 @@
 module Spec.QoS0.Broker where
 
-import Arbitrary
+import Arbitrary ()
 import Broker
+import Network.Socket ( Socket )
 import Test.Tasty (TestTree, testGroup)
+
+import Data.Maybe (isJust)
+
 import qualified Test.Tasty.QuickCheck as QC
 
 test :: TestTree 
 test = testGroup "Broker" [test_createMessagePackets]
 
+instance QC.Arbitrary Socket where
+  arbitrary = undefined
+
+
 test_createMessagePackets :: TestTree
 test_createMessagePackets = testGroup "createMessagePackets" [shouldIgnoreNothings]
   where
-    shouldIgnoreNothings = QC.testProperty "ignores Nothings" (\m subs -> isEmpty $ createMessagePackets m $ map (\sub -> (sub, Nothing)) subs)
-
-    isEmpty xs = length xs QC.=== 0
+    shouldIgnoreNothings = QC.testProperty "filters out unconnected subscriptions" 
+                                           (\m subs -> let justCount = length (filter (isJust . snd) subs)
+                                                       in length (createMessagePackets m subs) QC.=== justCount)
