15f00a4f53c6c11be6ba6f494ad0240c8f33bf61
JesseFloren <jessefloren@gmail.com>
2023-04-19 21:55:50 +0200

    Moved client to base and fixed import/exports

--- src-broker/Main.hs
+++ src-broker/Main.hs
@@ -4,5 +4,6 @@ import Broker (createBroker, BrokerConfig(..))
 main :: IO ()
 main = do
     createBroker (BrokerConfig 8000 (Just "supersecretpassword") 1000)
+    putStrLn "Started Broker on port 8000, press any key to close the broker"
     _ <- getLine
     putStrLn "Quit"
